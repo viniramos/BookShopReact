40f89052fb5830e674e34e1e1731575d47c53b7d
Rafael Januzzi <80737860+rjanuzzi@users.noreply.github.com>
2024-11-03 23:46:46 -0300

    Not Found

--- bookshop/src/pages/NotFound/NotFound.jsx
+++ bookshop/src/pages/NotFound/NotFound.jsx
@@ -1,15 +1,30 @@
+import React from 'react';
 import { useNavigate } from "react-router-dom"
+import styles from './NotFound.module.css';
 
-export function NotFound(){
 
-    const navigate = useNavigate()
-    const handleNavigation=()=> navigate("/")
+export function NotFound() {
+    const navigate = useNavigate();
+    const handleNavigation = () => navigate("/");
 
-    return(
+    return (
         <>
-            <h1>Error 404: Not Found</h1>
-
-            <button onClick={handleNavigation}>Voltar a página inicial</button>
+        <div className={styles.container}>
+            <nav className={styles.shelf}>
+                <div className={`${styles.book} ${styles.home_page}`} onClick={handleNavigation}>Home Page</div>
+                <div className={`${styles.book} ${styles.about_us}`}>Sobre Nós</div>
+                <div className={`${styles.book} ${styles.contact}`}>Contato</div>
+                <div className={`${styles.book} ${styles.faq}`}>Login</div>
+                
+                <div className={`${styles.book} ${styles.not_found}`}></div>
+                
+                <div className={`${styles.door} ${styles.left}`}></div>
+                <div className={`${styles.door} ${styles.right}`}></div>
+            </nav>
+            <h1 className={styles.not_found_title}>Error 404: Not Found</h1>
+            <p className={styles.not_found_subtitle}>Essá página não foi encontrada, favor selecione uma opção</p>
+            </div>
         </>
-    )
+        
+    );
 }
